539daffe782b9df37b33e382f07bee29bff2a695
yash-sonar-1 <sonaryash10@gmail.com>
2025-12-27 19:14:22 +0530

    Testing Second API

--- src/main/java/com/yash/backend/controller/HelloController.java
+++ src/main/java/com/yash/backend/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.yash.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello world , Backend is working!";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "Testing the Second API";
     }
 }
